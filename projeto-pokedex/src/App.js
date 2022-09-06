11368900e5320e5a72bf9cd182df520243ddf159
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-09-06 19:57:16 -0300

    navbar

--- projeto-pokedex/src/App.js
+++ projeto-pokedex/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from './routes/Router'
 import Heelloo from "./Heelloo"
 import SearchAppBar from './components/Navbar/Navbar';
+import { Card } from './components/CardPokemon/CardPokemon';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <SearchAppBar/>
       <Router />
       <Heelloo />
-  
+  <Card></Card>
     </div>
   );
 }
